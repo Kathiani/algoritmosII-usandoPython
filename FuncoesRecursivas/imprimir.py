305cf2267822b3ccd99d8a5775805e5bd5814c8c
Kathiani <kathianisouza@gmail.com>
2024-04-15 20:53:52 -0300

    adding recursive function

--- FuncoesRecursivas/imprimir.py
+++ FuncoesRecursivas/imprimir.py
@@ -1,9 +1,9 @@
 
-def imprimeListaAoContrario(lista):
+def imprimeLista(lista):
     print(lista)
     if len(lista)!= 0:
-        imprimeListaAoContrario(lista[:-1])
+        imprimeLista(lista[:-1])
 
 
 #chamando função
-imprimeListaAoContrario([9,8,7,6,5,4])
+imprimeLista([9,8,7,6,5,4,3])
