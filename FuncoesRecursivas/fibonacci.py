7ad145c8c59d9dfd795f1424fe050373226e9997
Kathiani <kathianisouza@gmail.com>
2024-04-15 22:38:09 -0300

    adding fibonacci

--- FuncoesRecursivas/fibonacci.py
+++ FuncoesRecursivas/fibonacci.py
@@ -1,14 +1,13 @@
-#fibonacci apenas imprimindo valores na sequência a partir da sequência fornecida
 def fibonacciv1(lista, n):
    if n!=0:
         print(lista[0] + lista[1])
         lista = [lista[1], lista[0] + lista[1]]
         n = n - 1
-        fibonacci(lista, n)
+        fibonacciv1(lista, n)
    else:
         print("fim da execução")
 
 
 
 #chamada da função
-fibonacci([1, 2], 10)
+fibonacciv1([1, 2], 10)  #fibonacci apenas imprimindo valores na sequência a partir da sequência fornecida
